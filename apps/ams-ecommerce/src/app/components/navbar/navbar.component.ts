73e900fa427b2aeb366d9afc9683f905beee1da2
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-04-19 09:55:16 +0000

    ref: add link to profile and logout

--- apps/ams-ecommerce/src/app/components/navbar/navbar.component.ts
+++ apps/ams-ecommerce/src/app/components/navbar/navbar.component.ts
@@ -2,27 +2,52 @@ import { Component, inject, Input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 import { AuthService } from '../../services/auth.service';
-import { CartItemComponent } from "../cart/cart-item.component";
+import { CartItemComponent } from '../cart/cart-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faUser,
+  faSignOut,
+  faArrowDown,
+  faArrowUp,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-    selector: 'app-navbar',
-    standalone: true,
-    templateUrl: './navbar.component.html',
-    styleUrl: './navbar.component.css',
-    imports: [RouterLink, RouterLinkActive, CartItemComponent]
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+  imports: [RouterLink, RouterLinkActive, CartItemComponent, FontAwesomeModule],
 })
 export class NavbarComponent {
+  userIcon = faUser;
+  logoutIcon = faSignOut;
+  arrowDownIcon = faArrowDown;
+  arrowUpIcon = faArrowUp;
+
   cartService = inject(CartService);
   authService = inject(AuthService);
-  open=false
-  numberOf(){
-    return this.cartService.numberOfProducts
+  openCart = false;
+  openProfile = false;
+  numberOf() {
+    return this.cartService.numberOfProducts;
   }
   checkAuth() {
-    return this.authService.checkAuth()
+    return this.authService.checkAuth();
   }
 
   hideCartedItemsCard() {
-    this.open = !this.open
+    this.openCart = !this.openCart;
   }
+
+  toggleProfile() {
+    this.openProfile = !this.openProfile;
+ 
+  }
+
+  handleLogout(){
+    localStorage.removeItem('user')
+  }
+
+ 
+  
 }
