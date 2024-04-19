1dddc0077a2e7e6a8b05604424ba102b1739a5b8
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-04-19 10:00:18 +0000

    ref: update my auth page

--- apps/ams-ecommerce/src/app/pages/auth/auth.component.ts
+++ apps/ams-ecommerce/src/app/pages/auth/auth.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiService } from '../../services/api.service';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -13,8 +15,10 @@ import { ApiService } from '../../services/api.service';
 export class AuthComponent implements OnInit {
   router = inject(Router)
   fb = inject(FormBuilder)
+  auth = inject(AuthService)
   constructor(private apiService: ApiService){}
   isLogin: boolean = true;
+  registerAuthSuccess$!:Object
   authForm = this.fb.group({
     firstName: '',
     lastName: '',
@@ -28,6 +32,8 @@ export class AuthComponent implements OnInit {
   currentUrl = this.router.url.split('/').pop();
 
   ngOnInit(): void {
+    
+    if(!this.auth.checkAuth()) this.router.navigate(['/'])
     this.isLogin = this.router.url.split('/').pop() === 'register' ? false : true;
   }
 
@@ -42,10 +48,26 @@ export class AuthComponent implements OnInit {
   handleAuthentication(){
    if(this.isLogin){
     console.log('auth')
-    this.apiService.login(this.authForm.value).subscribe(v=>console.log(v))
+    this.apiService.login(this.authForm.value).subscribe({
+      next:(v)=>{
+        localStorage.setItem('user', JSON.stringify(v))
+        this.router.navigate(['/'])
+      }
+    })
+    this.authForm.setValue({
+      firstName: '',
+      lastName: '',
+      email:'',
+      password:'',
+      correctPassword:''
+    })
    }
    if(!this.isLogin && this.displayError()){
-    this.apiService.register(this.authForm.value).subscribe(v=>console.log(v))
+    this.apiService.register(this.authForm.value).subscribe({next:()=>{
+      this.router.navigate(['/auth/login'])
+    }, error: ()=>{
+
+    }})
     this.authForm.setValue({
       firstName: '',
       lastName: '',
