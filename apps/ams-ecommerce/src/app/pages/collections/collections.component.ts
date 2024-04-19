bdb6ad82854d53fc5e10a804d2140c0c508bc55f
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-04-19 10:01:02 +0000

    ref: move components to product info

--- apps/ams-ecommerce/src/app/pages/collections/collections.component.ts
+++ apps/ams-ecommerce/src/app/pages/collections/collections.component.ts
@@ -1,45 +1,11 @@
-import { Component, inject } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
-import { CurrencyPipe, PercentPipe, UpperCasePipe } from '@angular/common';
-import { ImageBtnComponent } from '../../components/image-btn/image-btn.component';
-import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { CartService } from '../../services/cart.service';
-import { AuthService } from '../../services/auth.service';
-import { ProductService } from '../../services/product.service';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-collections',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ImageBtnComponent, UpperCasePipe, CurrencyPipe, PercentPipe],
+  imports: [],
   templateUrl: './collections.component.html',
-  styleUrl: './collections.component.css'
+  styleUrl: './collections.component.css',
 })
-export class CollectionsComponent {
-  router = inject(Router)
-  cartService = inject(CartService)
-  authService = inject(AuthService)
-  productService = inject(ProductService)
-  
-  products = this.productService.products
-  active = 0;
-  isActive = true;
-  handleIsActive(index: number){
-    return this.active === index
-  }
-  hangeProductChange(index: number) {
-    this.active = index
-  }
-  decrease(index:number) {
-    this.products.map((product, i)=>{
-      if(index === i) product.quantity--
-    })
-  }
-  increase(index:number) {
-    this.products.map((product, i)=>{
-      if(index === i) product.quantity++
-    })
-  }
-  handleAddToCart(productId: number) {
-    this.cartService.AddToCart(productId)
-  }
-}
+export class CollectionsComponent {}
