829757a846b11cec002f0143c5ed5edd7234e8b0
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-04-19 09:54:17 +0000

    ref: add routes

--- apps/ams-ecommerce/src/app/app.routes.ts
+++ apps/ams-ecommerce/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { CollectionsComponent } from './pages/collections/collections.component';
 import { MenSectionsComponent } from './pages/men-sections/men-sections.component';
 import { WomenSectionsComponent } from './pages/women-sections/women-sections.component';
@@ -7,13 +6,34 @@ import { LandingPageComponent } from './pages/landing-page/landing-page.componen
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { AuthComponent } from './pages/auth/auth.component';
 import { authGuard } from './guard/auth.guard';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { NewOrdersComponent } from './components/new-orders/new-orders.component';
+import { ProductsCartComponent } from './components/products-cart/products-cart.component';
 
 export const routes: Routes = [
-    { path: '', component: LandingPageComponent },
-    { path: 'collections', component: CollectionsComponent },
-    { path: 'men-section', component: MenSectionsComponent},
-    { path: 'women-section', component: WomenSectionsComponent, canActivate:[authGuard] },
-    { path: 'auth/login', component: AuthComponent },
-    { path: 'auth/register', component: AuthComponent },
-    { path: '**', component: NotFoundComponent },
+  { path: '', component: LandingPageComponent },
+  {
+    path: '',
+    children: [
+      { path: 'collections', component: CollectionsComponent },
+      { path: 'men-section', component: MenSectionsComponent },
+      { path: 'women-section', component: WomenSectionsComponent },
+      {path: 'profile', redirectTo: 'profile/info', pathMatch: 'full'},
+      {
+        path: 'profile', // Dynamic profile route with a user ID parameter
+        component: ProfileComponent,
+        children: [
+          {path: 'info', component: UserInfoComponent},
+          {path: 'new-orders', component: NewOrdersComponent},
+          {path: 'products-in-carts', component: ProductsCartComponent}
+        ] // Route to your ProfileComponent
+      },
+    ],
+    canActivate: [authGuard],
+  },
+
+  { path: 'auth/login', component: AuthComponent },
+  { path: 'auth/register', component: AuthComponent },
+  { path: '**', component: NotFoundComponent },
 ];
