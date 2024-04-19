3d8b8b078e73c822b0472026ae7688b74b90bd7f
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-04-19 10:01:40 +0000

    ref: update code to use the correct imported component

--- apps/ams-ecommerce/src/app/pages/landing-page/landing-page.component.ts
+++ apps/ams-ecommerce/src/app/pages/landing-page/landing-page.component.ts
@@ -1,16 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { CollectionsComponent } from "../collections/collections.component";
 import { ProductService } from '../../services/product.service';
-import { JsonPipe } from '@angular/common';
+
 import { ProductCardComponent } from "../../components/product-card/product-card.component";
 import { ReviewsCardComponent } from "../../components/reviews-card/reviews-card.component";
+import { ProductInfoComponent } from "../../components/product-info/product-info.component";
+
 
 @Component({
     selector: 'app-landing-page',
     standalone: true,
     templateUrl: './landing-page.component.html',
     styleUrl: './landing-page.component.css',
-    imports: [CollectionsComponent, ProductCardComponent, ReviewsCardComponent]
+    imports: [ProductCardComponent, ReviewsCardComponent, ProductInfoComponent]
 })
 export class LandingPageComponent {
     productService = inject(ProductService)
