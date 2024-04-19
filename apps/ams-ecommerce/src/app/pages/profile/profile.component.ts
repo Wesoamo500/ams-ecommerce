70259255d9800c2f76ad2effb3bc7d282f307ece
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-04-19 10:02:11 +0000

    feat: add profile page

---
+++ apps/ams-ecommerce/src/app/pages/profile/profile.component.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { ApiService } from '../../services/api.service';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
+  imports: [JsonPipe, AsyncPipe, RouterModule ]
+})
+export class ProfileComponent implements OnInit, OnDestroy {
+  apiService = inject(ApiService)
+  profileData$!: Observable<any>;
+  private routeSubscription: Subscription | null = null;
+
+  
+
+  ngOnInit() {
+      
+        this.fetchProfileData();
+      
+  }
+
+  ngOnDestroy() {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+  }
+
+  fetchProfileData() {
+    this.profileData$ = this.apiService.fetchProfileData()
+  }
+}
