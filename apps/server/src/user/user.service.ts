47f173fa7c9cca2a0cda448dee055c0b2f22caa5
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-04-22 08:27:15 +0000

    ref: implement the create user, hashPassword, update funtion

--- apps/server/src/user/user.service.ts
+++ apps/server/src/user/user.service.ts
@@ -1,19 +1,25 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
-import * as argon from 'argon2'
-
+import * as argon from 'argon2';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectRepository(User) private usersRepository: Repository<User>) { }
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>
+  ) {}
   async register(createUserDto: CreateUserDto) {
-    const user = await this.findByEmail(createUserDto.email)
-    if(user){
-      throw new HttpException("User Already Exists", HttpStatus.CONFLICT)
+    const user = await this.findByEmail(createUserDto.email);
+    if (user) {
+      throw new HttpException('User Already Exists', HttpStatus.CONFLICT);
     }
     return await this.createUser(createUserDto);
   }
@@ -22,33 +28,54 @@ export class UserService {
     return `This action returns all user`;
   }
 
-  findOne(id: number) {
-    return this.usersRepository.findOneBy({id})
+  async findOne(id: string) {
+    return await this.usersRepository.findOneBy({ id });
   }
 
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    let user = await this.findOne(id);
+    console.log({...updateUserDto})
+    if (!user) throw new NotFoundException('User not Found');
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    user.firstName = updateUserDto.firstName;
+    user.country = updateUserDto.country;
+    user.lastName = updateUserDto.lastName;
+    user.email = updateUserDto.email;
+    user.city = updateUserDto.city;
+    user.dateOfBirth = updateUserDto.dateOfBirth;
+    user.permanentAddress = updateUserDto.permanentAddress;
+    user.postalCode = updateUserDto.postalCode;
+    user.profileImage = updateUserDto.profileImage;
+
+    const updatedUser = await this.usersRepository.save(user);
+
+    console.log(updatedUser);
+    return updatedUser;
   }
 
   remove(id: number) {
     return `This action removes a #${id} user`;
   }
 
-  public async findByEmail(email: string){
+  public async findByEmail(email: string) {
     return await this.usersRepository.findOneBy({
-      email
-    })
+      email,
+    });
   }
 
-  private async createUser(dto: CreateUserDto){
-    const hashedPwd = await this.hashedPassword(dto.password)
-    const newUser =  this.usersRepository.create({...dto,hashedPassword: hashedPwd})
-    const {hashedPassword,...user} = await this.usersRepository.save(newUser)
-    return user
+  private async createUser(dto: CreateUserDto) {
+    const hashedPwd = await this.hashedPassword(dto.password);
+    const newUser = this.usersRepository.create({
+      ...dto,
+      hashedPassword: hashedPwd,
+    });
+    const { hashedPassword, ...user } = await this.usersRepository.save(
+      newUser
+    );
+    return user;
   }
 
-  private async hashedPassword(password: string){
-    return argon.hash(password)
+  private async hashedPassword(password: string) {
+    return argon.hash(password);
   }
 }
