b2ff1911464f5bd52f5dc5cb9ec30f3cb809d30e
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-04-22 08:27:51 +0000

    ref: update the create user dto

--- apps/server/src/user/dto/create-user.dto.ts
+++ apps/server/src/user/dto/create-user.dto.ts
@@ -1,6 +1,53 @@
+import { IsEmail, IsOptional, IsString } from "class-validator";
+
 export class CreateUserDto {
-    firstName: string
-    lastName: string
-    email: string
-    password: string
+  id: string;
+
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  hashedPassword: string;
+
+  @IsOptional()
+  @IsString()
+  permanentAddress: string;
+
+  @IsOptional()
+  @IsString()
+  country: string;
+
+  @IsOptional()
+  @IsString()
+  profileImage: string;
+
+  @IsOptional()
+  @IsString()
+  city: string;
+
+  @IsOptional()
+  @IsString()
+  username: string;
+
+  @IsOptional()
+  @IsString()
+  postalCode: string;
+
+  @IsOptional()
+  @IsString()
+  phoneNumber: string;
+
+  @IsOptional()
+  @IsString()
+  dateOfBirth: string;
 }
