b68bf219f4fe4e830c204c326d37d620dd06be19
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-04-24 13:20:16 +0000

    ref: add validation pipe

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
@@ -13,6 +13,7 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({}));
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
