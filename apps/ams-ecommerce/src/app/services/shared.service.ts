290e473b5aa0af120125796ff9c6e6765a1238d1
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-04-24 13:19:29 +0000

    ref: add the addresses behaviour subject

--- apps/ams-ecommerce/src/app/services/shared.service.ts
+++ apps/ams-ecommerce/src/app/services/shared.service.ts
@@ -1,10 +1,12 @@
 import { Injectable, signal } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
   openAddressModal = signal(false)
+  addresses = new BehaviorSubject<any[]>([])
   handleOpenAddressModal(){
     return this.openAddressModal.update((prev)=>!prev)
   }
