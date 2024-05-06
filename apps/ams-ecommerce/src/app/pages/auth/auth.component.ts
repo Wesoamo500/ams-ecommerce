3cfbfc154303517d56bb022a04abe914bac502df
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-05-06 13:37:03 +0000

    ref: add toastr feature

--- apps/ams-ecommerce/src/app/pages/auth/auth.component.ts
+++ apps/ams-ecommerce/src/app/pages/auth/auth.component.ts
@@ -2,8 +2,8 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiService } from '../../services/api.service';
-import { Observable } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-auth',
@@ -16,6 +16,7 @@ export class AuthComponent implements OnInit {
   router = inject(Router)
   fb = inject(FormBuilder)
   auth = inject(AuthService)
+  toastr = inject(ToastrService)
   constructor(private apiService: ApiService){}
   isLogin: boolean = true;
   registerAuthSuccess$!:Object
@@ -49,8 +50,12 @@ export class AuthComponent implements OnInit {
    if(this.isLogin){
     this.apiService.login(this.authForm.value).subscribe({
       next:(v)=>{
+        this.toastr.success('Login successful 😆','Login' )
         localStorage.setItem('user', JSON.stringify(v))
         this.router.navigate(['/'])
+      },
+      error: (err)=>{
+        this.toastr.error(err.error.message,err.error.error)
       }
     })
     this.authForm.setValue({
@@ -63,7 +68,7 @@ export class AuthComponent implements OnInit {
    }
    if(!this.isLogin && this.displayError()){
     this.apiService.register(this.authForm.value).subscribe({next:()=>{
-      this.router.navigate(['/auth/login'])
+      this.toastr.success('Login', 'Login successful😆')
     }, error: ()=>{
 
     }})
