7bb2ff49ecbd8b1b675a7e1922707129ecb1d7cb
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-05-08 11:20:48 +0000

    ref: update

--- apps/ams-ecommerce/src/app/services/product.service.ts
+++ apps/ams-ecommerce/src/app/services/product.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, OnInit } from '@angular/core';
 import { Products } from '../../data/products';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
+export class ProductService{
+  apiService = inject(ApiService)
+  products = Products 
 
-  constructor() { }
-
-  products = Products
+  fetchProducts(){
+    
+  }
 }
