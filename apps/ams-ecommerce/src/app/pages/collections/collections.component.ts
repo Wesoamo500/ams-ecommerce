c7962456d78164a9bcaf71f8ca9712d734d75ece
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-05-08 11:14:19 +0000

    ref: implement collections page

--- apps/ams-ecommerce/src/app/pages/collections/collections.component.ts
+++ apps/ams-ecommerce/src/app/pages/collections/collections.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
+import { ProductCardComponent } from "../../components/product-card/product-card.component";
 
 @Component({
-  selector: 'app-collections',
-  standalone: true,
-  imports: [],
-  templateUrl: './collections.component.html',
-  styleUrl: './collections.component.css',
+    selector: 'app-collections',
+    standalone: true,
+    templateUrl: './collections.component.html',
+    styleUrl: './collections.component.css',
+    imports: [AsyncPipe, JsonPipe, CommonModule, ProductCardComponent]
 })
-export class CollectionsComponent {}
+export class CollectionsComponent implements OnInit {
+  api = inject(ApiService);
+
+  products$!: Observable<any>;
+
+  ngOnInit(): void {
+    this.products$ = this.api.fetchProduct();
+  }
+}
