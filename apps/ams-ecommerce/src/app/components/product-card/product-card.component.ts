ace5a7ac9a5951780d701b9c91eff29451fbe1f5
Wesoamo500 <sulemanabdulmanan@gmail.com>
2024-05-08 11:13:14 +0000

    ref: import an icon

--- apps/ams-ecommerce/src/app/components/product-card/product-card.component.ts
+++ apps/ams-ecommerce/src/app/components/product-card/product-card.component.ts
@@ -1,15 +1,18 @@
 import { CurrencyPipe, NgFor } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { Product } from '../../../data/types/product';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBasketShopping } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CurrencyPipe, NgFor],
+  imports: [CurrencyPipe, NgFor, FontAwesomeModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
-  @Input() product!: Product;
-  @Input() index!:number
+  @Input() product!: any;
+  @Input() index!:number;
+
+  cartIcon = faBasketShopping
 }
